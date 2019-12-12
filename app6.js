a9f964f355659bfdd142ff01ec0981f20ccd7c62
Ilya Desktop <ig@pss.one>
2019-12-12 12:24:00 +0300

    for in

--- app6.js
+++ app6.js
@@ -1,6 +1,6 @@
 //while, do while, for, for of, for in
 
-let i=0;
+//let i=0;
 //while(i++<10){
 //  console.log(i);  
 //}
@@ -11,17 +11,15 @@ let i=0;
 //  i++;
 //};
 
- for (let i = 0; i<10; i++){
-   
-   if(i ===5){
-      continue;
-   }
-
-   if(i===8){
-     break;
-   }
-  console.log(i)
-}
+//  for (let i = 0; i<10; i++){
+//    if(i ===5){
+//       continue;
+//    }
+//    if(i===8){
+//      break
+//    }
+//   console.log(i)
+// }
 
 
 //let str = 'Hello';
@@ -35,3 +33,41 @@ let i=0;
 // for(let i =0;i<colors.length;i++){    
 // console.log(colors[i]);
 // }
+
+// const users = [{
+//   name: 'Denis',
+//   age: 20
+// },
+// {
+//   name: 'Igor',
+//   age: 30
+// },
+// {
+//   name: 'Vasya',
+//   age: 27
+// },
+// {
+//   name: 'Ildar',
+//   age: 30
+// }  
+// ];
+
+// const usersObj ={};
+
+// for(let i=0;i<users.length;i++){
+//   console.log(users[i].name);
+
+//   usersObj[users[i].name] = users[i];
+// }
+
+// console.log(usersObj);
+
+const user = {
+  name: 'Denis',
+  age: 30
+};
+
+for( let key in user){
+  console.log(key);
+  console.log(user[key]);
+}
